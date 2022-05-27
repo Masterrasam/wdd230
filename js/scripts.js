5740b96074f866af1409da261b05258cc468008a
Masterrasam <Samuelokoosi21@gmail.com>
2022-05-27 17:32:05 +0100

    update chambers

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
-/* Get Current Date for Header */
 const currentDateSpan = document.querySelector("#currentDate");
-
 const now = new Date();
+
 currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
   dateStyle: "full",
 }).format(now);
 
-
 /* hamburger toggle menu */
 function toggleMenu() {
-  document.getElementsByClassName("navbar")[0].classList.toggle("respond");
+    document.querySelector("#mainNav").classList.toggle("open");
+    document.querySelector("#menuButton").classList.toggle("open");
 }
+const x = document.querySelector("#menuButton");
+x.onclick = toggleMenu;
+
 
 /* last modified */
 let nd = new Date();
 
 document.getElementById("currentYear").textContent = nd.getFullYear();
-
 document.querySelector("#modDate").textContent = document.lastModified;
+
+/* Banner on Mondays & Tuesdays */
+if(now.getDay() === 2) {
+    document.getElementById("banner").style.display = "block";
+}
+const close = document.querySelector("#close");
+close.addEventListener("click", () => {
+    banner.style.display = "none";
+});
