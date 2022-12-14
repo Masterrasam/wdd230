91ae8a09c353c002bf25ecaec26ae66834f08e45
Masterrasam <samuelokoosi21@gmail.com>
2022-12-14 13:20:05 +0100

    update

--- week 12 final/js/menu-script.js
+++
@@ -1,6 +0,0 @@
-//* HAMBURGER MENU 
-//* Function for responsive hamburger menu on small display
-
-function hamburgerMenu(){
-	document.getElementsByClassName("menu")[0].classList.toggle("responsive");
-}
